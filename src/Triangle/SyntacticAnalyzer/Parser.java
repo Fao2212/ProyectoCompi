782d197072688c1652652018766a779359faa17b
Xfighter32 <austinhakanson2@gmail.com>
2021-09-20 07:12:24 -0600

    Se elimina del singleCommand el case para el keyword BEGIN, también se agregan métodos de parseo para compoundDeclaration, procFuncDeclaration y procFuncsDeclaration

--- src/Triangle/SyntacticAnalyzer/Parser.java
+++ src/Triangle/SyntacticAnalyzer/Parser.java
@@ -49,6 +49,7 @@ import Triangle.AbstractSyntaxTrees.IntegerExpression;
 import Triangle.AbstractSyntaxTrees.IntegerLiteral;
 import Triangle.AbstractSyntaxTrees.LetCommand;
 import Triangle.AbstractSyntaxTrees.LetExpression;
+import Triangle.AbstractSyntaxTrees.LocalDeclaration;
 import Triangle.AbstractSyntaxTrees.MultipleActualParameterSequence;
 import Triangle.AbstractSyntaxTrees.MultipleArrayAggregate;
 import Triangle.AbstractSyntaxTrees.MultipleFieldTypeDenoter;
@@ -62,6 +63,7 @@ import Triangle.AbstractSyntaxTrees.Program;
 import Triangle.AbstractSyntaxTrees.RecordAggregate;
 import Triangle.AbstractSyntaxTrees.RecordExpression;
 import Triangle.AbstractSyntaxTrees.RecordTypeDenoter;
+import Triangle.AbstractSyntaxTrees.RecursiveProcFuncDeclaration;
 import Triangle.AbstractSyntaxTrees.SequentialCommand;
 import Triangle.AbstractSyntaxTrees.SequentialDeclaration;
 import Triangle.AbstractSyntaxTrees.SimpleTypeDenoter;
@@ -82,6 +84,7 @@ import Triangle.AbstractSyntaxTrees.VarInitializedDeclaration;
 import Triangle.AbstractSyntaxTrees.Vname;
 import Triangle.AbstractSyntaxTrees.VnameExpression;
 import Triangle.AbstractSyntaxTrees.WhileCommand;
+import jdk.nashorn.internal.runtime.regexp.joni.Syntax;
 
 public class Parser {
 
@@ -292,12 +295,6 @@ public class Parser {
           commandAST = new AssignCommand(vAST, eAST, commandPos);
         }
       }
-      break;
-
-    case Token.BEGIN:
-      acceptIt();
-      commandAST = parseCommand();
-      accept(Token.END);
       break;
 
     case Token.LET:
@@ -602,6 +599,47 @@ public class Parser {
     return declarationAST;
   }
 
+  Declaration parseCompoundDeclaration() throws SyntaxError {
+    Declaration declarationAST = null; // in case there's a syntactic error
+
+    SourcePosition declarationPos = new SourcePosition();
+    start(declarationPos);
+
+    switch (currentToken.kind) {
+    
+    case Token.RECURSIVE:
+      {
+        acceptIt();
+        Declaration dAST = parseProcFuncsDeclaration();
+        accept(Token.END);
+        finish(declarationPos);
+        declarationAST = new RecursiveProcFuncDeclaration(dAST, declarationPos);
+      }
+      break;
+    
+    case Token.LOCAL:
+      {
+        acceptIt();
+        Declaration d1AST = parseDeclaration();
+        accept(Token.IN);
+        Declaration d2AST = parseDeclaration();
+        accept(Token.END);
+        finish(declarationPos);
+        declarationAST = new LocalDeclaration(d1AST, d2AST, declarationPos);
+      }
+      break;
+    case Token.CONST: case Token.VAR: case Token.PROC:
+    case Token.FUNC: case Token.TYPE:
+    declarationAST = parseSingleDeclaration();
+    
+    default:
+      syntacticError("\"%\" cannot start a compound declaration",
+        currentToken.spelling);
+      break;
+    }
+    return declarationAST;
+  }
+
   Declaration parseSingleDeclaration() throws SyntaxError {
     Declaration declarationAST = null; // in case there's a syntactic error
 
@@ -694,6 +732,70 @@ public class Parser {
     return declarationAST;
   }
 
+  Declaration parseProcFuncsDeclaration() throws SyntaxError {
+    Declaration declarationAST = null; // in case there's a syntactic error
+
+    SourcePosition declarationPos = new SourcePosition();
+    start(declarationPos);
+
+    declarationAST = parseProcFuncDeclaration();
+    do {
+      accept(Token.PIPE);
+      Declaration d2AST = parseProcFuncDeclaration();
+      declarationAST = new SequentialDeclaration(declarationAST, d2AST, declarationPos);
+    }
+    while (currentToken.kind == Token.PIPE);
+    finish(declarationPos);
+    return declarationAST;
+  }
+
+  Declaration parseProcFuncDeclaration() throws SyntaxError {
+    Declaration declarationAST = null; // in case there's a syntactic error
+
+    SourcePosition declarationPos = new SourcePosition();
+    start(declarationPos);
+
+    switch (currentToken.kind) {
+
+    case Token.PROC:
+      {
+        acceptIt();
+        Identifier iAST = parseIdentifier();
+        accept(Token.LPAREN);
+        FormalParameterSequence fpsAST = parseFormalParameterSequence();
+        accept(Token.RPAREN);
+        accept(Token.IS);
+        Command cAST = parseCommand();
+        accept(Token.END);
+        finish(declarationPos);
+        declarationAST = new ProcDeclaration(iAST, fpsAST, cAST, declarationPos);
+      }
+      break;
+    
+    case Token.FUNC:
+      {
+        acceptIt();
+        Identifier iAST = parseIdentifier();
+        accept(Token.LPAREN);
+        FormalParameterSequence fpsAST = parseFormalParameterSequence();
+        accept(Token.RPAREN);
+        accept(Token.COLON);
+        TypeDenoter tAST = parseTypeDenoter();
+        accept(Token.IS);
+        Expression eAST = parseExpression();
+        accept(Token.END);
+        finish(declarationPos);
+        declarationAST = new FuncDeclaration(iAST, fpsAST, tAST, eAST, declarationPos);
+      }
+      break;
+
+    default:
+      syntacticError("\"%\" cannot start a mutually reursive procedure or a function",
+        currentToken.spelling);
+      break;
+    }
+    return declarationAST;
+  }
 ///////////////////////////////////////////////////////////////////////////////
 //
 // PARAMETERS
